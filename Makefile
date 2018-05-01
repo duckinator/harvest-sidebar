3ea81c424ec7adf4358d11ffd1e55d67fd9d3371
Ellen Marie Dash <me@duckie.co>
2018-05-01 18:27:03 -0400

    ?????

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: build/harvest-sidebar.zip
 
 build/harvest-sidebar.zip:
 	mkdir -p build/
-	zip -r build/harvest-sidebar.zip ./sidebar.js manifest.json
+	zip -r build/harvest-sidebar.zip sidebar.js manifest.json
 
 clean:
 	rm -f build/*.zip
--- sidebar.js
+++ sidebar.js
@@ -1,34 +1,46 @@
-function updateSidebar() {
-    var currentTab = browser.tabs.getCurrent();
-    var currentUrl = currentTab.url;
-    var urlParts = url.split("/");
+var iframe = null;
+
+function addSidebar() {
+    if (iframe != null) {
+      return;
+    }
+
+    var currentUrl = location.href;
+    var urlParts = currentUrl.split("/");
     var itemId;
+
+    var repo = urlParts.slice(3, 5).join("/");
+
+    // Projects being tracked.
+    if (["how-is/how_is","rubygems/rubygems"].indexOf(repo) == -1) {
+      return;
+    }
+
+    // No actual issue number is specified.
+    if (urlParts.length != 7) {
+      return;
+    }
 
     // If it's not an issue or PR, don't do anything.
     if (urlParts[5] == "issues" || urlParts[5] == "pulls") {
         // <user>/<repo>/<issue or pull>/<issue or pr number>
         itemId = urlParts.slice(3, 7).join("/");
     } else {
-        // 
         itemId = urlParts.slice(3, 5).join("/");
     }
 
     var externalItemId = encodeURIComponent(itemId);
-    var externalItemName = encodeURIComponent(currentTab.title);
+    var externalItemName = encodeURIComponent(document.title);
     var permalink = encodeURIComponent(currentUrl);
 
-    var url = "https://platform.harvestapp.com/platform/timer?external_item_id=how-is%2Fhow_is%23227&external_item_name=%23227%3A%20resolve%20codeclimate%20issues&permalink=https%3A%2F%2Fgithub.com%2Fhow-is%2Fhow_is%2Fissues%2F227&closeable=false&default_project_code=HowIs&default_project_code=HowIs";
-    var url = "https://platform.harvestapp.com/platform/timer?external_item_id=" + externalitemId + "&external_item_name=" + externalItemName + "&permalink=" + permalink;
- 
-   browser.sidebarAction.setPanel({
-        "tabId": currentTab.id,
-        "panel": url,
-    });
-    browser.sidebarAction.open();
+    var url = "https://platform.harvestapp.com/platform/timer?external_item_id=" + externalItemId + "&external_item_name=" + externalItemName + "&permalink=" + permalink + "#" + repo;
+
+    iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.style = "position: fixed; left: 0; top: 0; height: 100%;";
+    document.body.appendChild(iframe);
+
+    document.addEventListener("load", addSidebar);
 }
 
-// New tab is activated.
-browser.tabs.onActivated.addListener(updateSidebar);
-
-// New page is loaded.
-browser.tabs.onUpdated.addListener(updateSidebar);
+addSidebar();
